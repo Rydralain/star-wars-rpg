ba027f4b5846e57b1d9b3d5647df2815cf8023de
Rydralain <37129740+Rydralain@users.noreply.github.com>
2018-05-29 19:14:22 -0700

    Basic JS & starting character class setup

---
+++ js/game.js
@@ -0,0 +1,16 @@
+// template for characters
+var character = function(hp, ap, cap){
+    this.maxHP = hp;
+    this.minAP = ap;
+    this.cap = cap;
+
+    
+}
+
+
+
+
+
+$( document ).ready(function() {
+    
+});
