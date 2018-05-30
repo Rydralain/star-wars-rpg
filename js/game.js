653c6f16d15e648108faa97ce139c4f91ad02017
Rydralain <37129740+Rydralain@users.noreply.github.com>
2018-05-29 19:54:59 -0700

    HTML Structure, classes, and some more JS for game and characters

--- js/game.js
+++ js/game.js
@@ -1,12 +1,61 @@
 // template for characters
-var character = function(hp, ap, cap){
+var character = function(name, hp, ap, cap){
+    this.name = name;
     this.maxHP = hp;
     this.minAP = ap;
     this.cap = cap;
 
-    
+    this.selectDefender(){
+        this.selectFighter();
+        // move to defender space
+    }
+    this.selectAttacker(){
+        this.selectFighter();
+        //move to attacker space
+    }
+    this.selectFighter = function(){
+        this.currentHP = this.maxHP;
+        this.currentAP = this.minAP;
+
+    }
+    this.attack = function(){
+        var apNow = this.currentAP;
+        this.currentAP++;
+        return apNow;
+    }
+    this.takeDamage = function(damage){
+        this.currentHP -= damage;
+        if(this.currentHP <= 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
 
+// game object
+var game = {
+    "inactiveFighters" : 4,
+    "obiwan" : new character("Obiwan", 6, 4, 4),
+    "anakin" : new character("Anakin", 11, 3, 3),
+    "maul" : new character("Maul", 15, 2, 2),
+    "savage" : new character("Savage", 20, 1, 1),
+    "currentAttacker" : "",
+    "currentDefender" : "",
+    "setAttacker" : function(attacker){
+        game.currentAttacker = attacker;
+        game[attacker].selectAttacker();
+    },
+    "setDefender" : function(defender){
+        game.currentDefender = defender;
+        game[defender].selectDefender();
+    },
+    "runAttacks" : function(){
+        attackDamage = game[game.currentAttacker].attack();
+    }
+
+}
 
 
 
