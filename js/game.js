f812f8635483b6bfd480694ff1f297b9e4d44338
Rydralain <37129740+Rydralain@users.noreply.github.com>
2018-05-30 23:50:24 -0700

    Added images

--- js/game.js
+++ js/game.js
@@ -4,12 +4,14 @@ var character = function(name, hp, ap, cap){
     this.maxHP = hp;
     this.minAP = ap;
     this.cap = cap;
+    
+    $("#game").append('<img id="'+this.name+'" class="character-image" src="./images/'+this.name+'.png">');
 
-    this.selectDefender(){
+    this.selectDefender = function(){
         this.selectFighter();
         // move to defender space
     }
-    this.selectAttacker(){
+    this.selectAttacker = function(){
         this.selectFighter();
         //move to attacker space
     }
@@ -23,6 +25,9 @@ var character = function(name, hp, ap, cap){
         this.currentAP++;
         return apNow;
     }
+    this.getCAP = function(){
+        return this.cap;
+    }
     this.takeDamage = function(damage){
         this.currentHP -= damage;
         if(this.currentHP <= 0){
@@ -34,32 +39,47 @@ var character = function(name, hp, ap, cap){
     }
 }
 
-// game object
-var game = {
-    "inactiveFighters" : 4,
-    "obiwan" : new character("Obiwan", 6, 4, 4),
-    "anakin" : new character("Anakin", 11, 3, 3),
-    "maul" : new character("Maul", 15, 2, 2),
-    "savage" : new character("Savage", 20, 1, 1),
-    "currentAttacker" : "",
-    "currentDefender" : "",
-    "setAttacker" : function(attacker){
-        game.currentAttacker = attacker;
-        game[attacker].selectAttacker();
-    },
-    "setDefender" : function(defender){
-        game.currentDefender = defender;
-        game[defender].selectDefender();
-    },
-    "runAttacks" : function(){
-        attackDamage = game[game.currentAttacker].attack();
-    }
-
-}
 
 
 
 
 $( document ).ready(function() {
-    
+    // game object
+    var game = {
+        "inactiveFighters" : 4,
+        "obiwan" : new character("Obiwan", 6, 4, 4),
+        "anakin" : new character("Anakin", 11, 3, 3),
+        "maul" : new character("Maul", 15, 2, 2),
+        "savage" : new character("Savage", 20, 1, 1),
+        "currentAttacker" : "",
+        "currentDefender" : "",
+        "setAttacker" : function(attacker){
+            game.currentAttacker = attacker;
+            game[attacker].selectAttacker();
+        },
+        "setDefender" : function(defender){
+            game.currentDefender = defender;
+            game[defender].selectDefender();
+        },
+        "runAttacks" : function(){
+            var attacker = game[game.currentAttacker];
+            var defender = game[game.currentDefender];
+
+            var attackDamage = attacker.attack();
+            if(defender.takeDamage(attackDamage)){
+                console.log("die")
+            }
+            else{
+                console.log(attackDamage);
+            }
+
+            var defenseDamage = defender.getCAP();
+        }
+
+    }
+
+
+
+    game.setDefender("anakin");
+    game.setAttacker("savage");
 });
