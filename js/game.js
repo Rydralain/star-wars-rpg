8609c642e9700b7724869893d5924d1bc4685e3e
Rydralain <37129740+Rydralain@users.noreply.github.com>
2018-05-31 00:07:23 -0700

    Initial animations

--- js/game.js
+++ js/game.js
@@ -5,7 +5,8 @@ var character = function(name, hp, ap, cap){
     this.minAP = ap;
     this.cap = cap;
     
-    $("#game").append('<img id="'+this.name+'" class="character-image" src="./images/'+this.name+'.png">');
+    $("#game").append('<img onclick="game.choose('+this.name+')" id="'+this.name+'" class="character-image" src="./images/'+this.name+'.png">');
+    this.myDiv = $("#"+this.name);
 
     this.selectDefender = function(){
         this.selectFighter();
@@ -37,6 +38,38 @@ var character = function(name, hp, ap, cap){
             return false;
         }
     }
+
+    this.goToPosition = function (position) {
+        var newLeft = 0;
+        var newTop = 0;
+        switch(position){
+            case 0:
+                newTop = 30;
+                newLeft = 50;
+                break;
+            case 1:
+                newTop = 30;
+                newLeft = 155;
+                break;
+            case 2:
+                newTop = 30;
+                newLeft = 260;
+                break;
+            case 3:
+                newTop = 30;
+                newLeft = 365
+                break;
+            case 4:
+                newTop = 170;
+                newLeft = 100;
+                break;
+            case 5:
+                newTop = 170;
+                newLeft = 300;
+                break;
+        }
+        this.myDiv.animate({left: newLeft, top: newTop});
+    }
 }
 
 
@@ -46,7 +79,6 @@ var character = function(name, hp, ap, cap){
 $( document ).ready(function() {
     // game object
     var game = {
-        "inactiveFighters" : 4,
         "obiwan" : new character("Obiwan", 6, 4, 4),
         "anakin" : new character("Anakin", 11, 3, 3),
         "maul" : new character("Maul", 15, 2, 2),
@@ -56,10 +88,12 @@ $( document ).ready(function() {
         "setAttacker" : function(attacker){
             game.currentAttacker = attacker;
             game[attacker].selectAttacker();
+            game[attacker].goToPosition(4);
         },
         "setDefender" : function(defender){
             game.currentDefender = defender;
             game[defender].selectDefender();
+            game[defender].goToPosition(5);
         },
         "runAttacks" : function(){
             var attacker = game[game.currentAttacker];
@@ -74,12 +108,18 @@ $( document ).ready(function() {
             }
 
             var defenseDamage = defender.getCAP();
+        },
+        "setup" : function(){
+            game.obiwan.goToPosition(0);
+            game.anakin.goToPosition(1);
+            game.maul.goToPosition(2);
+            game.savage.goToPosition(3);
+        },
+        "choose" : function(){
+            
         }
 
     }
 
-
-
-    game.setDefender("anakin");
-    game.setAttacker("savage");
+    game.setup();
 });
